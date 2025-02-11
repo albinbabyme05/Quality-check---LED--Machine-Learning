128ab4ec49eef8aa62026dcf5259e5983fcbe79a
Albin Baby <albinbabayme05@gmail.com>
2025-02-11 23:24:18 +0100

    Add function to compute ORB descriptors for loaded images

--- Qulaity Check/main.py
+++ Qulaity Check/main.py
@@ -16,3 +16,15 @@ for item in images_list:
     class_names.append(os.path.splitext(item)[0])
 
 print("Loaded images:", class_names)
+
+
+def descriptFinder(images):
+    """Find descriptors for each loaded image using ORB"""
+    descriptor_list = []
+    for image in images:
+        keypoint, descriptor = orb.detectAndCompute(image, None)
+        descriptor_list.append(descriptor)
+    return descriptor_list
+
+descriptor_list = descriptFinder(images=images)
+print(f"Total descriptors extracted: {len(descriptor_list)}")
